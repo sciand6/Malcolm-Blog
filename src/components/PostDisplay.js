38caf2dbe0500357c442917a475dff6e64e3f4f7
Malcolm Sciandra <sciand6@gmail.com>
2019-09-03 19:47:05 -0400

    Simplified some operations.

--- src/components/PostDisplay.js
+++ src/components/PostDisplay.js
@@ -17,23 +17,24 @@ class PostDisplay extends Component {
         // Get post by id
         axios.get(`http://localhost:4000/posts/${this.props.match.params.id}`)
             .then(post => {
+                let { title, author, content, postDate } = post.data;
                 this.setState({ // Still thinking about how I can just reduce this down to just this: 'this.setState({ post.data });'
-                    title: post.data.title,
-                    content: post.data.content,
-                    author: post.data.author,
-                    postDate: post.data.postDate
+                    title: title,
+                    content: content,
+                    author: author,
+                    postDate: postDate
                 })
             }); 
     }
 
     render() {
-        // Rendering the posting here
-        let posting = this.state;
+        // Rendering the here
+        let { title, author, content, postDate } = this.state;
         return (
             <div className="container">
-                <h2>{posting.title}</h2>
-                <p>Posted by <strong>{posting.author}</strong> on: <em>{posting.postDate}</em></p>
-                <div dangerouslySetInnerHTML={{ __html: posting.content }}></div>
+                <h2>{title}</h2>
+                <p>Posted by <strong>{author}</strong> on: <em>{postDate}</em></p>
+                <div dangerouslySetInnerHTML={{ __html: content }}></div>
             </div>
         );
     }
